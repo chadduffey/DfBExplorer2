a1e751e9008c815fad17afb5e506941a5a15981a
Chad Duffey <chadduffey@hotmail.com>
2015-04-19 20:30:03 +1000

    initial

---
+++ models.py
@@ -0,0 +1,33 @@
+import datetime
+
+from flask.ext.bcrypt import generate_password_hash
+from flask.ext.login import UserMixin
+from peewee import *
+
+DATABASE = SqliteDatabase('dfbexplorer.db')
+
+class User(UserMixin, Model):
+	username = CharField(unique=True)
+	email = CharField(unique=True)
+	password = CharField(max_length=100)
+	joined_at = DateTimeField(default=datetime.datetime.now)
+	is_admin = BooleanField(default=False)
+	#this will hold the users dropbox auth key
+	dropboxKey = CharField(max_length=200)
+	#this is just in case we need it during development.
+	dropboxField2 = CharField(max_length=200)
+
+	class Meta:
+		database = DATABASE
+		order_by = ('-joined_at',)
+
+	@classmethod
+	def create_user(cls, username, email, password, admin=False):
+		try:
+			cls.create(
+				username = username,
+				email=email,
+				password=generate_password_hash(password),
+				is_admin=admin)
+		except IntegrityError:
+			raise ValueError("User Already Exists")
